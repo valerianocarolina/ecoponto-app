6189cf90909a767fb0658740cf369b2a2f02cda1
Carolina Valeriano <valeriano.carolina@icloud.com>
2026-03-23 19:41:32 +0000

    fix: next config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  allowedDevOrigins: ["*.csb.app"],
 };
 
 export default nextConfig;
